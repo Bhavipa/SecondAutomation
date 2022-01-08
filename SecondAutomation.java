2a3d018e32fdfc347977ca62dab0d39527b56aac
bhavita <bhavu4444@gmail.com>
2022-01-08 12:40:17 +0000

    my commit

---
+++ SecondAutomation.java
@@ -0,0 +1,10 @@
+package com.secondautomation;
+
+public class SecondAutomation {
+    public static void main(String[]args)
+    {
+        System.out.println("What Is Ide?");
+        System.out.println("Packages Is Folder Where We Collected All Items");
+
+    }
+}
