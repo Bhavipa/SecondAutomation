c01e45d4d25df0b3fafd2bccd154188f8305e1e1
bhavita <bhavu4444@gmail.com>
2022-01-10 17:07:08 +0000

    my commit

--- SecondAutomation.java
+++ SecondAutomation.java
@@ -3,9 +3,12 @@ package com.secondautomation;
 public class SecondAutomation {
     public static void main(String[]args)
     {
-        System.out.println("What Is Ide?");
+
         System.out.println("Packages Is Folder Where We Collected All Items");
         System.out.println("Class is Represent Real world entitles");
+        System.out.println("Git And Github Are Different");
+        System.out.println("Java Features");
+        System.out.println("Naming Conventions");
 
 
     }
